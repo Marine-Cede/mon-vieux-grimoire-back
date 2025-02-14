fe54cdd8a40dcde0499ee9868bb8b721333ca488
Marine Cede <marine.cedee@gmail.com>
2025-02-14 12:22:32 +0100

    feat: ajout du sharp

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -8,18 +8,13 @@ const MIME_TYPES = {
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    console.log("destination")
     callback(null, 'images');
-    console.log("destination FAite")
 
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    console.log("name",name)
-    console.log("extension",extension);
     callback(null, name + Date.now() + '.' + extension);
-    console.log("filename Fait")
   }
 });
 
--- routes/book.js
+++ routes/book.js
@@ -2,12 +2,13 @@ const express = require('express');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/book');
+const sharper = require('../middleware/sharp-config');
 const router = express.Router();
 
 router.get('/',  bookCtrl.getAllBooks);
 router.get('/:id',  bookCtrl.getOneBook);
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.post('/', auth, multer, sharper, bookCtrl.createBook);
+router.put('/:id', auth, multer,sharper, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
 
